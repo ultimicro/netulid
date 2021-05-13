bb9cd7e0b3c7626a182842e886297a45b009e0eb
Putta Khunchalee <ultimaweapon@outlook.com>
2021-05-13 13:22:06 +0700

    Adds NetUlid.Tool

---
+++ src/NetUlid.Tool/GenerateCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace NetUlid.Tool
+{
+    using System;
+    using System.CommandLine;
+    using System.CommandLine.Invocation;
+    using System.Threading.Tasks;
+
+    internal sealed class GenerateCommand : Command, ICommandHandler
+    {
+        public GenerateCommand()
+            : base("gen", "Generate a new ULID")
+        {
+            this.Handler = this;
+        }
+
+        public Task<int> InvokeAsync(InvocationContext context)
+        {
+            Console.WriteLine(Ulid.Generate());
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/NetUlid.Tool/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NetUlid.Tool
+{
+    using System.CommandLine;
+    using System.Threading.Tasks;
+
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var root = new RootCommand("ULID tool for .NET Core.")
+            {
+                new GenerateCommand(),
+            };
+
+            return await root.InvokeAsync(args);
+        }
+    }
+}
