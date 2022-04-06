4f87eb774797309f4a2ae97617d2ba16bf7e3755
Putta Khunchalee <ultimaweapon@outlook.com>
2022-04-06 23:12:01 +0700

    Upgrades to .NET 6

--- src/NetUlid/UlidConverter.cs
+++ src/NetUlid/UlidConverter.cs
@@ -6,12 +6,12 @@
 
     public sealed class UlidConverter : TypeConverter
     {
-        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
         {
             return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
         }
 
-        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
         {
             switch (value)
             {
